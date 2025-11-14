7eca126bd59c5f60ef7978beee946f70a72d91f5
seyeon-night <seyeon-night@gmail.com>
2025-11-14 14:06:44 +0900

    미사용 파일 삭제

--- Main.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="코로나 대시보드 미션",
-                   page_icon="")
-
-st.title('코로나 대시보드')
--- pages_/Vaccines.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="백신 접종 현황",
-                   page_icon="💊")
-
-st.header('백신 접종 현황')
